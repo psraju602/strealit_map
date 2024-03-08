b104a3e8f2209f8bf52a3b5975bba77730623525
psraju602 <162550240+psraju602@users.noreply.github.com>
2024-03-08 16:59:17 +0530

    Update bank_live.py

--- bank_live.py
+++ bank_live.py
@@ -2,6 +2,7 @@ import time  # to simulate a real time data, time loop
 
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
+import plotly
 import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
 
