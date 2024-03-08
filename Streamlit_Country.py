181b49dac153d8cf2fcbf3f823a1dbb37652f59a
psraju602 <162550240+psraju602@users.noreply.github.com>
2024-03-08 16:04:14 +0530

    Update Streamlit_Country.py

--- Streamlit_Country.py
+++ Streamlit_Country.py
@@ -1,20 +1,14 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
 
-i=1
-while i<=10:
-    df = pd.DataFrame({
-        "col1": np.random.randn(1000) / 50 + 37.76,
-        "col2": np.random.randn(1000) / 50 + -122.4,
-        "col3": np.random.randn(1000) * 100,
-        "col4": np.random.rand(1000, 4).tolist()
-    })
+df = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist()
+})
 
 
-    st.map(df,
-        latitude='col1',
-        longitude='col2',
-        size='col3',
-        color='col4')
-    i = i+1
+st.map(df,
+    latitude='col1',
+    longitude='col2',
+    size='col3',
+    color='col4')
