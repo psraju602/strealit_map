65a239a944292306108803a600588db5584f7c75
psraju602 <162550240+psraju602@users.noreply.github.com>
2024-03-08 22:20:19 +0530

    Update Streamlit_Country2.py

--- Streamlit_Country2.py
+++ Streamlit_Country2.py
@@ -6,16 +6,17 @@ import time
 # Create a container to hold the map
 map_container = st.empty()
  
+# Create the DataFrame outside the loop
+df = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist()
+})
+
+# Run the loop to update the map
 for seconds in range(200):
     with map_container.container():
-
- 
-        df = pd.DataFrame({
-            "col1": np.random.randn(1000) / 50 + 37.76,
-            "col2": np.random.randn(1000) / 50 + -122.4,
-            "col3": np.random.randn(1000) * 100,
-            "col4": np.random.rand(1000, 4).tolist()
-        })
         # Update the content of the container with the map
         st.map(df,
             latitude='col1',
