4a382ff9c72d158a91c5310bbf7fe151cdeee7d5
psraju602 <162550240+psraju602@users.noreply.github.com>
2024-03-11 21:53:13 +0530

    Update Map11.py

--- Map11.py
+++ Map11.py
@@ -1,232 +1,76 @@
-import time  # to simulate a real time data, time loop
+import streamlit as st # web development
+import numpy as np # np mean, np random 
+import pandas as pd # read csv, df manipulation
+import time # to simulate a real time data, time loop 
+import plotly.express as px # interactive charts 
 
-import numpy as np  # np mean, np random
-import pandas as pd  # read csv, df manipulation
-import plotly
-import plotly.express as px  # interactive charts
-import streamlit as st  # 🎈 data web app development
 
-#dataset_url = "psraju602/strealit_map/bank.csv"
-dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
-# read csv from a URL
-@st.experimental_memo
-def get_data() -> pd.DataFrame:
-    return pd.read_csv(dataset_url)
+# read csv from a github repo
+df = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
 
-df = get_data()
 
 st.set_page_config(
-    page_title="Real-Time Data Science Dashboard",
-    page_icon="✅",
-    layout="wide",
+    page_title = 'Real-Time Data Science Dashboard',
+    page_icon = '✅',
+    layout = 'wide'
 )
 
 # dashboard title
-#st.title("Real-Time / Live Data Science Dashboard")
 
-# top-level filters
-job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
+st.title("Real-Time / Live Data Science Dashboard")
 
+# top-level filters 
 
-# create three columns
-kpi1, kpi2, kpi3 = st.columns(3)
+job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
 
-# fill in those three columns with respective metrics or KPIs
-kpi1.metric(
-    label="Age ⏳",
-    value=round(avg_age),
-    delta=round(avg_age) - 10,
-)
-
-kpi2.metric(
-    label="Married Count 💍",
-    value=int(count_married),
-    delta=-10 + count_married,
-)
-
-kpi3.metric(
-    label="A/C Balance ＄",
-    value=f"$ {round(balance,2)} ",
-    delta=-round(balance / count_married) * 100,
-)
-
-# create two columns for charts
-fig_col1, fig_col2 = st.columns(2)
-
-with fig_col1:
-    st.markdown("### First Chart")
-    fig = px.density_heatmap(
-        data_frame=df, y="age_new", x="marital"
-    )
-    st.write(fig)
-
-with fig_col2:
-    st.markdown("### Second Chart")
-    fig2 = px.histogram(data_frame=df, x="age_new")
-    st.write(fig2)
-
-st.markdown("### Detailed Data View")
-st.dataframe(df)
-
-for seconds in range(200):
-
-    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
-    time.sleep(1)
 
 # creating a single-element container.
 placeholder = st.empty()
 
-with placeholder.container():
-    # create three columns
-    kpi1, kpi2, kpi3 = st.columns(3)
+# dataframe filter 
 
-    # fill in those three columns with respective metrics or KPIs
-    kpi1.metric(
-        label="Age ⏳",
-        value=round(avg_age),
-        delta=round(avg_age) - 10,
-    )
+df = df[df['job']==job_filter]
 
-    kpi2.metric(
-        label="Married Count 💍",
-        value=int(count_married),
-        delta=-10 + count_married,
-    )
+# near real-time / live feed simulation 
 
-    kpi3.metric(
-        label="A/C Balance ＄",
-        value=f"$ {round(balance, 2)} ",
-        delta=-round(balance / count_married) * 100,
-    )
-
-    # create two columns for charts
-    fig_col1, fig_col2 = st.columns(2)
-
-    with fig_col1:
-        st.markdown("### First Chart")
-        fig = px.density_heatmap(
-            data_frame=df, y="age_new", x="marital"
-        )
-        st.write(fig)
-
-    with fig_col2:
-        st.markdown("### Second Chart")
-        fig2 = px.histogram(data_frame=df, x="age_new")
-        st.write(fig2)
-
-    st.markdown("### Detailed Data View")
-    st.dataframe(df)
-    time.sleep(1)
-
-import time  # to simulate a real time data, time loop
-
-import numpy as np  # np mean, np random
-import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
-import streamlit as st  # 🎈 data web app development
-
-st.set_page_config(
-    page_title="Real-Time Data Science Dashboard",
-    page_icon="✅",
-    layout="wide",
-)
-
-# read csv from a github repo
-dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
-
-
-# read csv from a URL
-@st.experimental_memo
-def get_data() -> pd.DataFrame:
-    return pd.read_csv(dataset_url)
-
-
-df = get_data()
-
-# dashboard title
-st.title("Real-Time / Live Data Science Dashboard")
-
-# top-level filters
-job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
-
-# creating a single-element container
-placeholder = st.empty()
-
-# dataframe filter
-df = df[df["job"] == job_filter]
-
-# near real-time / live feed simulation
 for seconds in range(200):
-    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+#while True: 
+    
+    df['age_new'] = df['age'] * np.random.choice(range(1,5))
+    df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
 
-    # creating KPIs
-    avg_age = np.mean(df["age_new"])
+    # creating KPIs 
+    avg_age = np.mean(df['age_new']) 
 
-    count_married = int(
-        df[(df["marital"] == "married")]["marital"].count()
-        + np.random.choice(range(1, 30))
-    )
-
-    balance = np.mean(df["balance_new"])
+    count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
+    
+    balance = np.mean(df['balance_new'])
 
     with placeholder.container():
         # create three columns
         kpi1, kpi2, kpi3 = st.columns(3)
 
-        # fill in those three columns with respective metrics or KPIs
-        kpi1.metric(
-            label="Age ⏳",
-            value=round(avg_age),
-            delta=round(avg_age) - 10,
-        )
+        # fill in those three columns with respective metrics or KPIs 
+        kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
+        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
+        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
 
-        kpi2.metric(
-            label="Married Count 💍",
-            value=int(count_married),
-            delta=-10 + count_married,
-        )
+        # create two columns for charts 
 
-        kpi3.metric(
-            label="A/C Balance ＄",
-            value=f"$ {round(balance, 2)} ",
-            delta=-round(balance / count_married) * 100,
-        )
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            center = [20.7128, 78.0060]
+            fig2 = st.map(df_map,latitude='col1',longitude='col2', size='col3', color='col4')
+            st.write(fig2)
 
-# create two columns for charts
-fig_col1, fig_col2 = st.columns(2)
-with fig_col1:
-    st.markdown("### First Chart")
-    fig = px.density_heatmap(
-        data_frame=df, y="age_new", x="marital"
-    )
-    st.write(fig)
+            
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
+    #placeholder.empty()
 
-'''
-with fig_col2:
-    st.markdown("### Second Chart")
-    fig2 = px.histogram(data_frame=df, x="age_new")
-    st.write(fig2)
- '''
-df_map = pd.DataFrame({
-            "col1": np.random.randn(1000) / 50 + 37.76,
-            "col2": np.random.randn(1000) / 50 + -122.4,
-            "col3": np.random.randn(1000) * 100,
-            "col4": np.random.rand(1000, 4).tolist()
-        })
-with fig_col2:
-    st.markdown("### Second Chart")
-    center = [20.7128, 78.0060]
-    #fig2 = px.choropleth_mapbox(data_frame=df_map, x="age_new")
-
-    fig2 = st.map(df_map,
-           latitude='col1',
-           longitude='col2',
-           size='col3',
-           color='col4')
-    st.write(fig2)
-
-st.markdown("### Detailed Data View")
-st.dataframe(df)
-time.sleep(1)
